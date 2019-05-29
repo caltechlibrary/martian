836000257a7941fd7b0b0f5e7e5bbefb421d2ce0
Michael Hucka <mhucka@caltech.edu>
2019-05-29 12:39:29 -0700

    Add initial __init__.py file

---
+++ martian/__init__.py
@@ -0,0 +1,20 @@
+'''
+Martian -- Search caltech.tind.io with a user query and download MARC records
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2018 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+from .__version__ import __version__, __title__, __name__
+from .__version__ import __description__, __url__
+from .__version__ import __author__, __email__
+from .__version__ import __license__, __copyright__
