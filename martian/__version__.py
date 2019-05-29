118fd1b29488e2ce057c82527d53e9aac67df358
Michael Hucka <mhucka@caltech.edu>
2019-05-29 12:38:32 -0700

    Add version file

---
+++ martian/__version__.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   Martian version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/martian
+# =============================================================================
+
+__version__     = '0.0.1'
+__title__       = 'martian'
+__name__        = 'Martian'
+__description__ = '''A program for Caltech Library staff to download MARC records from caltech.tind.io.'''
+__url__         = 'https://github.com/caltechlibrary/martian'
+__author__      = 'Michael Hucka <mhucka@caltech.edu>'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD 3-clause license -- see LICENSE file'
+__copyright__   = 'Copyright (C) 2018 by the California Institute of Technology'
